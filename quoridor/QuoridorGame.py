e7b3b977217bb3fb1dc011e964c1008dabeeaddd
lockcept <lockcept@gmail.com>
2023-12-11 04:31:31 +0900

    feat: quoridor board 구현

--- quoridor/QuoridorGame.py
+++ quoridor/QuoridorGame.py
@@ -1,74 +1,61 @@
 from __future__ import print_function
 import sys
+from quoridor.action import QuoridorAction
+
+from quoridor.board import QuoridorBoard
 
 sys.path.append("..")
 from Game import Game
-from .QuoridorLogic import Board
 import numpy as np
 
 
 class QuoridorGame(Game):
-    square_content = {-1: "X", +0: "-", +1: "O"}
-
-    @staticmethod
-    def getSquarePiece(piece):
-        return QuoridorGame.square_content[piece]
-
     def __init__(self, n):
         self.n = n
 
     def getInitBoard(self):
         # return initial board (numpy board)
-        b = Board(self.n)
-        return np.array(b.pieces)
+        b = QuoridorBoard(size=self.n)
+        return b.pieces
 
     def getBoardSize(self):
-        # (a,b) tuple
-        return (self.n, self.n)
+        return (self.n, self.n, 6)
 
     def getActionSize(self):
-        # return number of actions
-        return self.n * self.n + 1
+        return self.n * self.n + 2 * (self.n - 1) * (self.n - 1)
 
-    def getNextState(self, board, player, action):
-        # if player takes action on board, return next (board,player)
-        # action must be a valid move
-        if action == self.n * self.n:
-            return (board, -player)
-        b = Board(self.n)
+    def getNextState(self, board, player: int, action: int):
+        b = QuoridorBoard(size=self.n)
         b.pieces = np.copy(board)
-        move = (int(action / self.n), action % self.n)
-        b.execute_move(move, player)
-        return (b.pieces, -player)
+        action = QuoridorAction.all_actions[action]
+        b.action_player(action, player)
+        return (b.pieces, 3 - player)
 
-    def getValidMoves(self, board, player):
+    def getValidMoves(self, board, player: int):
         # return a fixed size binary vector
         valids = [0] * self.getActionSize()
-        b = Board(self.n)
+        allActions = QuoridorAction.all_actions
+        b = QuoridorBoard(size=self.n)
         b.pieces = np.copy(board)
-        legalMoves = b.get_legal_moves(player)
-        if len(legalMoves) == 0:
+        validActions = b.get_legal_moves(player)
+        if len(validActions) == 0:
             valids[-1] = 1
             return np.array(valids)
-        for x, y in legalMoves:
-            valids[self.n * x + y] = 1
+        for action, index in allActions:
+            if b.is_valid_action(player=player, action=action):
+                valids[index] = 1
         return np.array(valids)
 
-    def getGameEnded(self, board, player):
+    def getGameEnded(self, board, player: int):
         # return 0 if not ended, 1 if player 1 won, -1 if player 1 lost
         # player = 1
-        b = Board(self.n)
+        b = QuoridorBoard(self.n)
         b.pieces = np.copy(board)
-        if b.has_legal_moves(player):
-            return 0
-        if b.has_legal_moves(-player):
-            return 0
-        if b.countDiff(player) > 0:
-            return 1
-        return -1
+        return b.is_game_ended(player=player)
 
-    def getCanonicalForm(self, board, player):
-        # return state if player==1, else return -state if player==-1
+    def getCanonicalForm(self, board, player: int):
+        b = QuoridorBoard(self.n)
+        b.pieces = np.copy(board)
         return player * board
 
     def getSymmetries(self, board, pi):
@@ -87,33 +74,14 @@ class QuoridorGame(Game):
                 l += [(newB, list(newPi.ravel()) + [pi[-1]])]
         return l
 
-    def stringRepresentation(self, board):
-        return board.tostring()
-
-    def stringRepresentationReadable(self, board):
-        board_s = "".join(
-            self.square_content[square] for row in board for square in row
-        )
-        return board_s
+    def stringRepresentation(self, board: QuoridorBoard):
+        return board.state_str()
 
     def getScore(self, board, player):
-        b = Board(self.n)
+        b = QuoridorBoard(self.n)
         b.pieces = np.copy(board)
         return b.countDiff(player)
 
     @staticmethod
-    def display(board):
-        n = board.shape[0]
-        print("   ", end="")
-        for y in range(n):
-            print(y, end=" ")
-        print("")
-        print("-----------------------")
-        for y in range(n):
-            print(y, "|", end="")  # print the row #
-            for x in range(n):
-                piece = board[y][x]  # get the piece to print
-                print(QuoridorGame.square_content[piece], end=" ")
-            print("|")
-
-        print("-----------------------")
+    def display(board: QuoridorBoard):
+        print(board.state_str())
