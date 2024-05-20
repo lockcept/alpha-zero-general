316762f75eae2b9631d58836c12a7466c3605268
lockcept <lockcept@gmail.com>
2024-05-20 20:14:35 +0900

    feat: add skeleton code

--- quoridor/QuoridorGame.py
+++ quoridor/QuoridorGame.py
@@ -8,21 +8,34 @@ class QuoridorGame(Game):
         self.n = n
 
     def getInitBoard(self):
-        # 초기 보드 상태를 반환
         b = Board(self.n)
-        return b
+        return b.to_array()
 
     def getBoardSize(self):
-        # 보드 크기 반환
-        return (self.n, self.n)
+        return (
+            self.n,
+            self.n,
+            6,
+        )  # p1_pos, p2_pos, h_walls, v_walls, p1_walls, p2_walls
 
     def getActionSize(self):
-        # 가능한 행동의 수 반환 (플레이어 이동 + 벽 설치)
         return self.n * self.n + (self.n - 1) * (self.n - 1) * 2
 
-    def getNextState(self, board, player, action):
-        # 주어진 행동에 따른 다음 상태 반환
-        b = board.clone()
+    def getNextState(self, board, player: int, action: int):
+        # valid action만 들어온다고 가정
+        b = Board(self.n)
+        b.p1_pos = tuple(np.argwhere(board[:, :, 0] == 1)[0])
+        b.p2_pos = tuple(np.argwhere(board[:, :, 1] == 1)[0])
+        b.p1_walls = int(board[0, 0, 4])
+        b.p2_walls = int(board[0, 0, 5])
+
+        for i in range(self.n):
+            for j in range(self.n):
+                if board[i, j, 2] == 1:
+                    b.h_walls.add((i, j))
+                if board[i, j, 3] == 1:
+                    b.v_walls.add((i, j))
+
         if action < self.n * self.n:
             move = (action // self.n, action % self.n)
             b.execute_move(move, player)
@@ -31,12 +44,23 @@ class QuoridorGame(Game):
             wall_pos = (action - self.n * self.n) % ((self.n - 1) * (self.n - 1))
             wall_pos = (wall_pos // (self.n - 1), wall_pos % (self.n - 1))
             b.place_wall(wall_pos, wall_type, player)
-        return (b, -player)
+        return (b.to_array(), -player)
 
     def getValidMoves(self, board, player):
         # 주어진 상태에서 가능한 행동 반환
         valids = [0] * self.getActionSize()
-        b = board.clone()
+        b = Board(self.n)
+        b.p1_pos = tuple(np.argwhere(board[:, :, 0] == 1)[0])
+        b.p2_pos = tuple(np.argwhere(board[:, :, 1] == 1)[0])
+        b.p1_walls = int(board[0, 0, 4])
+        b.p2_walls = int(board[0, 0, 5])
+
+        for i in range(self.n):
+            for j in range(self.n):
+                if board[i, j, 2] == 1:
+                    b.h_walls.add((i, j))
+                if board[i, j, 3] == 1:
+                    b.v_walls.add((i, j))
 
         # 이동 가능 체크
         moves = b.get_legal_moves(player)
@@ -56,21 +80,83 @@ class QuoridorGame(Game):
         return np.array(valids)
 
     def getGameEnded(self, board, player):
-        # 게임 종료 여부 판단
-        if board.is_win(player):
+        b = Board(self.n)
+        b.p1_pos = tuple(np.argwhere(board[:, :, 0] == 1)[0])
+        b.p2_pos = tuple(np.argwhere(board[:, :, 1] == 1)[0])
+        b.p1_walls = int(board[0, 0, 4])
+        b.p2_walls = int(board[0, 0, 5])
+
+        for i in range(self.n):
+            for j in range(self.n):
+                if board[i, j, 2] == 1:
+                    b.h_walls.add((i, j))
+                if board[i, j, 3] == 1:
+                    b.v_walls.add((i, j))
+
+        if b.is_win(player):
             return 1
-        if board.is_win(-player):
+        if b.is_win(-player):
             return -1
         return 0
 
     def getCanonicalForm(self, board, player):
         # 표준 형태의 보드 반환
-        return board
+        if player == 1:
+            return board
+        else:
+            new_board = np.zeros_like(board)
+            new_board[:, :, 0:2] = np.flip(board[:, :, 0:2], axis=[0, 2])
+            new_board[:-1, :-1, 2:4] = np.flip(board[:-1, :-1, 2:4], axis=0)
+            new_board[:, :, 4:6] = np.flip(board[:, :, 4:6], axis=2)
+            return new_board
 
     def getSymmetries(self, board, pi):
         # 대칭 상태 반환
-        return [(board, pi)]
+        assert len(pi) == self.getActionSize()  # 올바른 pi 길이 확인
+
+        pi_board = np.reshape(pi[: self.n * self.n], (self.n, self.n))
+        pi_walls = pi[self.n * self.n :]
+
+        l = []
+
+        for i in range(1, 5):
+            newB = np.rot90(board, i)
+            newPi = np.rot90(pi_board, i)
+            newWalls = pi_walls.copy()
+            if i % 2 == 1:
+                newWalls[: len(newWalls) // 2], newWalls[len(newWalls) // 2 :] = (
+                    newWalls[len(newWalls) // 2 :],
+                    newWalls[: len(newWalls) // 2],
+                )
+            newPi = list(newPi.ravel()) + list(newWalls)
+            l += [(newB, newPi)]
+
+        return l
 
     def stringRepresentation(self, board):
         # 보드 상태를 문자열로 반환
-        return board.to_string()
+        return board.tostring()
+
+    @staticmethod
+    def display(board: Board):
+        n = board.shape[0]
+        print("  ", end="")
+        for y in range(n):
+            print(y, end=" ")
+        print("")
+
+        for x in range(n):
+            print(x, end=" ")
+            for y in range(n):
+                piece = board[x, y, :]
+                if piece[0] == 1:
+                    print("1", end=" ")
+                elif piece[1] == 1:
+                    print("2", end=" ")
+                elif piece[2] == 1:
+                    print("-", end=" ")
+                elif piece[3] == 1:
+                    print("|", end=" ")
+                else:
+                    print(".", end=" ")
+            print("")
--- quoridor/QuoridorLogic.py
+++ quoridor/QuoridorLogic.py
@@ -1,24 +1,15 @@
+import numpy as np
+
+
 class Board:
     def __init__(self, n):
         self.n = n
-        self.board = [[0] * n for _ in range(n)]
         self.p1_pos = (0, n // 2)
         self.p2_pos = (n - 1, n // 2)
         self.p1_walls = 10
         self.p2_walls = 10
         self.h_walls = set()
         self.v_walls = set()
-
-    def clone(self):
-        b = Board(self.n)
-        b.board = [row[:] for row in self.board]
-        b.p1_pos = self.p1_pos
-        b.p2_pos = self.p2_pos
-        b.p1_walls = self.p1_walls
-        b.p2_walls = self.p2_walls
-        b.h_walls = self.h_walls.copy()
-        b.v_walls = self.v_walls.copy()
-        return b
 
     def execute_move(self, move, player):
         if player == 1:
@@ -40,4 +31,89 @@ class Board:
         if player == 1:
             return self.p1_pos[0] == self.n - 1
         else:
-            return self.p2_pos[0]
+            return self.p2_pos[0] == 0
+
+    def is_legal_move(self, pos, player):
+        if pos[0] < 0 or pos[0] >= self.n or pos[1] < 0 or pos[1] >= self.n:
+            return False
+        if player == 1:
+            current_pos = self.p1_pos
+        else:
+            current_pos = self.p2_pos
+
+        if pos == (current_pos[0] - 1, current_pos[1]) and (
+            (pos[0], pos[1]) in self.h_walls or (pos[0], pos[1] - 1) in self.h_walls
+        ):
+            return False
+        if pos == (current_pos[0] + 1, current_pos[1]) and (
+            (current_pos[0], current_pos[1]) in self.h_walls
+            or (current_pos[0], current_pos[1] - 1) in self.h_walls
+        ):
+            return False
+        if pos == (current_pos[0], current_pos[1] - 1) and (
+            (pos[0], pos[1]) in self.v_walls or (pos[0] - 1, pos[1]) in self.v_walls
+        ):
+            return False
+        if pos == (current_pos[0], current_pos[1] + 1) and (
+            (current_pos[0], current_pos[1]) in self.v_walls
+            or (current_pos[0] - 1, current_pos[1]) in self.v_walls
+        ):
+            return False
+
+        return True
+
+    def get_legal_moves(self, player):
+        if player == 1:
+            pos = self.p1_pos
+        else:
+            pos = self.p2_pos
+
+        possible_moves = [
+            (pos[0] - 1, pos[1]),
+            (pos[0] + 1, pos[1]),
+            (pos[0], pos[1] - 1),
+            (pos[0], pos[1] + 1),
+        ]
+        legal_moves = [
+            move for move in possible_moves if self.is_legal_move(move, player)
+        ]
+        return legal_moves
+
+    def is_legal_wall(self, pos, wall_type):
+        if pos[0] < 0 or pos[0] >= self.n - 1 or pos[1] < 0 or pos[1] >= self.n - 1:
+            return False
+        if wall_type == 0 and (
+            pos in self.h_walls
+            or (pos[0], pos[1] - 1) in self.h_walls
+            or (pos[0], pos[1] + 1) in self.h_walls
+        ):
+            return False
+        if wall_type == 1 and (
+            pos in self.v_walls
+            or (pos[0] - 1, pos[1]) in self.v_walls
+            or (pos[0] + 1, pos[1]) in self.v_walls
+        ):
+            return False
+        return True
+
+    def get_legal_walls(self, player):
+        legal_walls = []
+        for i in range(self.n - 1):
+            for j in range(self.n - 1):
+                if self.is_legal_wall((i, j), 0):
+                    legal_walls.append((i, j, "h"))
+                if self.is_legal_wall((i, j), 1):
+                    legal_walls.append((i, j, "v"))
+        return legal_walls
+
+    def to_array(self):
+        board_array = np.zeros((self.n, self.n, 6))
+        board_array[self.p1_pos][0] = 1
+        board_array[self.p2_pos][1] = 1
+        for wall in self.h_walls:
+            board_array[wall][2] = 1
+        for wall in self.v_walls:
+            board_array[wall][3] = 1
+        board_array[:, :, 4] = self.p1_walls
+        board_array[:, :, 5] = self.p2_walls
+        return board_array
